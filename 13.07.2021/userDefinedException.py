77bf11e9e4ce9c9dc0d83ec2cc7144c176c98c54
unknown <beeyasin2014@gmail.com>
2021-07-13 18:44:32 +0530

    13/07/2021

---
+++ 13.07.2021/userDefinedException.py
@@ -0,0 +1,16 @@
+class Error(Exception):
+	def __init__(self,msg):
+		self.msg = msg
+
+try:	
+	#print(10/0)
+	if(int(input("Enter Your age: "))>=18):
+		print("Can be vote")
+	else:
+		raise(Error("Below 18 can't be vote"))
+except Error as e:
+	print("Warning--> Age limit: ",e.msg)
+		
+finally:
+	print("End of test")
+	
