42036fd79f291d8e85bf7a8edf2e5afd279a9c47
unknown <beeyasin2014@gmail.com>
2021-07-14 17:59:56 +0530

    14/07/2021

--- 14.07.2021/inheritance.py
+++ 14.07.2021/inheritance.py
@@ -1,11 +1,49 @@
 class Bank():
 	
 
-	__name = "Reserve Bankk of India"
-	__Tax = "the standard consept 2%"
+	__name = "Reserve Bank of India"
+	__tax = "the standard 2%"
+	
+
+	#print(__name)		
+
+	
+	
+	def __repr__(self):
+		return(f"The bank  name is {self.__name} and the intrest rate is {self.__tax}")
+
+
+#print(b.__name)
+
+
+class Iob(Bank):
 
 	def __init__(self,name,tax):
 		self.name = name
 		self.tax = tax
+
 	
-	def __repr__()
+	def __repr__(self):
+		return(f"The bank  name is {self.name} and the intrest rate is {self.tax}")
+
+class Sbi(Iob):
+	def __init__(self,name,tax):
+		self.name = name
+		self.tax = tax
+
+	
+	def __repr__(self):
+		return(f"The bank  name is {self.name} and the intrest rate is {self.tax}")
+
+class Canara(Iob,Bank):
+	def __init__(self,name,tax):
+		self.name = name
+		self.tax = tax
+
+	
+	def __repr__(self):
+		return(f"The bank  name is {self.name} and the intrest rate is {self.tax}")
+
+c = Canara("Canara","4%")
+
+print(repr(c))
