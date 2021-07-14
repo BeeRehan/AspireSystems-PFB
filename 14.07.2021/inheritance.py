53a854dc8ac674a217b001ec1f60f6b0246e7017
unknown <beeyasin2014@gmail.com>
2021-07-14 12:47:59 +0530

    14/07/2021

---
+++ 14.07.2021/inheritance.py
@@ -0,0 +1,11 @@
+class Bank():
+	
+
+	__name = "Reserve Bankk of India"
+	__Tax = "the standard consept 2%"
+
+	def __init__(self,name,tax):
+		self.name = name
+		self.tax = tax
+	
+	def __repr__()
