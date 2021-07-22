6c857bdda69958e205efd1642b3e15b3830b64c7
unknown <beeyasin2014@gmail.com>
2021-07-22 18:48:24 +0530

    22/07/2021

--- 22.07.2021/Python/main.py
+++ 22.07.2021/Python/main.py
@@ -1,146 +1,132 @@
+from pprint import pprint
 from product.Items import *
 
-def init():
-    global add, view, delete 
-    obj = Products([{
-    "id": "1",
-    "name": "Iphone 11",
-    "product_details": {
-    "ram": "2 GB",
-    "processor": "snapdragon",
-    "screen_size": "4 inch",
-    },
-    "cost": "50000",
-    "currency": "INR",
-    "category": "mobile",
-    "colour": "black"
-    },
-    {
-    "id": "2",
-    "name": "Samsung Galaxy",
-    "product_details": {
-    "ram": "2 GB",
-    "processor": "snapdragon",
-    "screen_size": "4 inch",
-    },
-    "cost": "45000",
-    "currency": "INR",
-    "category": "mobile",
-    "colour": "grey"
-    },
-    {
-    "id": "3",
-    "name": "Washing Machine",
-    "product_details": {
-    "machine_capacity": "6kg",
-    "machine_rpm": "50",
-    "type": "top_load"
-    },
-    "cost": "25000",
-    "currency": "INR",
-    "category": "washing-machine",
-    "colour": "blue"
-    },
-    {
-    "id": "4",
-    "name": "Samsung TV",
-    "product_details": {
-    "isSmart": "false",
-    "resolution": "UHD",
-    "screen_size": "55 inch",
-    },
-    "cost": "40000",
-    "currency": "INR",
-    "category": "tv",
-    "colour": "black"
-    }])
+obj = Products([{
+"id": "1",
+"name": "Iphone 11",
+"product_details": {
+"ram": "2 GB",
+"processor": "snapdragon",
+"screen_size": "4 inch",
+},
+"cost": "50000",
+"currency": "INR",
+"category": "mobile",
+"colour": "black"
+},
+{
+"id": "2",
+"name": "Samsung Galaxy",
+"product_details": {
+"ram": "2 GB",
+"processor": "snapdragon",
+"screen_size": "4 inch",
+},
+"cost": "45000",
+"currency": "INR",
+"category": "mobile",
+"colour": "grey"
+},
+{
+"id": "3",
+"name": "Washing Machine",
+"product_details": {
+"machine_capacity": "6kg",
+"machine_rpm": "50",
+"type": "top_load"
+},
+"cost": "25000",
+"currency": "INR",
+"category": "washing-machine",
+"colour": "blue"
+},
+{
+"id": "4",
+"name": "Samsung TV",
+"product_details": {
+"isSmart": "false",
+"resolution": "UHD",
+"screen_size": "55 inch",
+},
+"cost": "40000",
+"currency": "INR",
+"category": "tv",
+"colour": "black"
+}])
+
+# def addMobile():
+#     product = dict()
+#     #print(product)
+#     product['id'] = input("Enter the ID: ")
+#     product['name'] = input("Enter the Name: ")
+#     product.update({'product_details':{'ram':input("Enter the Ram: "),'processor':input("Enter the processor: "),'screen_size':input("Enter the screensize: ")}})
+#     product['cost'] = input("Enter the cost: ")
+#     product['currency'] = input("Enter the currency: ")
+#     product['category'] = input("Enter the category: ")
+#     product['colour'] = input("Enter the colour: ")
     
-    add  = obj.add
-    view = obj.display
-    delete = obj.delete
+#     add(product)
+#     view()
+#     #print(product)
 
+# def addWashingMachine():
+#     product = dict()
+#     #print(product)
+#     product['id'] = input("Enter the ID: ")
+#     product['name'] = "Washing Machine"
+#     product.update({'product_details':{'machine_capacity:':input("Enter the machine_capacity: "),'machine_rpm':input("Enter the machine_rpm: "),'type':input("Enter the type: ")}})
+#     product['cost'] = input("Enter the cost: ")
+#     product['currency'] = input("Enter the currency: ")
+#     product['category'] = input("Enter the category: ")
+#     product['colour'] = input("Enter the colour: ")
+#     add(product)
+#     view()
 
-init()
+# def addTV():
+#     product = dict()
+#     #print(product)
+#     product['id'] = input("Enter the ID: ")
+#     product['name'] = input("Enter the Name: ")
+#     product.update({'product_details':{'isSmart':input("Enter the isSmart: "),'resolution':input("Enter the resolution: "),'    ':input("Enter the screensize: ")}})
+#     product['cost'] = input("Enter the cost: ")
+#     product['currency'] = input("Enter the currency: ")
+#     product['category'] = input("Enter the category: ")
+#     product['colour'] = input("Enter the colour: ")
+#     add(product)
+#     view()
 
-def select(msg):
-    return(input(msg))
-
-def addMobile():
-    product = dict()
-    #print(product)
-    product['id'] = input("Enter the ID: ")
-    product['name'] = input("Enter the Name: ")
-    product.update({'product_details':{'ram':input("Enter the Ram: "),'processor':input("Enter the processor: "),'screen_size':input("Enter the screensize: ")}})
-    product['cost'] = input("Enter the cost: ")
-    product['currency'] = input("Enter the currency: ")
-    product['category'] = input("Enter the category: ")
-    product['colour'] = input("Enter the colour: ")
-    
-    add(product)
-    view()
-    #print(product)
-
-def addWashingMachine():
-    product = dict()
-    #print(product)
-    product['id'] = input("Enter the ID: ")
-    product['name'] = "Washing Machine"
-    product.update({'product_details':{'machine_capacity:':input("Enter the machine_capacity: "),'machine_rpm':input("Enter the machine_rpm: "),'type':input("Enter the type: ")}})
-    product['cost'] = input("Enter the cost: ")
-    product['currency'] = input("Enter the currency: ")
-    product['category'] = input("Enter the category: ")
-    product['colour'] = input("Enter the colour: ")
-    add(product)
-    view()
-
-def addTV():
-    product = dict()
-    #print(product)
-    product['id'] = input("Enter the ID: ")
-    product['name'] = input("Enter the Name: ")
-    product.update({'product_details':{'isSmart':input("Enter the isSmart: "),'resolution':input("Enter the resolution: "),'    ':input("Enter the screensize: ")}})
-    product['cost'] = input("Enter the cost: ")
-    product['currency'] = input("Enter the currency: ")
-    product['category'] = input("Enter the category: ")
-    product['colour'] = input("Enter the colour: ")
-    add(product)
-    view()
+#obj.add('mobiles')
+obj.display()
 
 def addProduct():
     print("1. Mobile \n2. Washing Machine\n3. TV ")
     ch = input()
-    
     if(ch =='1'):
-        addMobile()
+        pprint(obj.add('mobiles'))
     elif(ch =='2'):
-        addWashingMachine()
+        pprint(obj.add('WashingMachines'))
     elif(ch=='3'):
-        addTV()
-
+        pprint(obj.add('tvs'))
+    else:
+        print('-------------------')
 
 def deleteProduct():    
     id = input("Enter the id: ")
-    delete(id)
-
+    obj.delete(id)
 
 def admin():
-    view()
+    #obj.display()
 
     while True:
         print("1. Add Product \n2. Delete Product")
         ch = input()
-
         if(ch=='1'):
             addProduct()
         elif(ch=='2'):
             deleteProduct()
 
-
-
-
 def customer():
      print("Under Construction!!!")
-
 
 while True:
     print("Who are you?")
@@ -150,13 +136,3 @@ while True:
         admin()
     elif(ch=='2'):
         customer()
-
-
-#dispaly()
-
-# if __name__ == '__main__':
-#     print("Welcome!!!")
-#     init()
-# else:
-#     print("!!!")
-#     print("Imported")
--- 22.07.2021/Python/product/Items.py
+++ 22.07.2021/Python/product/Items.py
@@ -1,4 +1,12 @@
 class Products:
+
+    crendials = {
+      'commons':['id','name','cost','currency','category','colour'],
+      'mobiles' : ['ram','processor','screen_size'],
+      'WashingMachines' : ['machine_capacity','machine_rpm','type'],
+      'tvs' : ['isSmart','resolution','screen_size'],
+    }
+
     def __init__(self,products):
         self.products = products
     
@@ -7,70 +15,29 @@ class Products:
         for product in self.products:
             print(product)
         
-    def add(self,product):
-        self.products.append(product)
+    def add(self,ind):
+        #print("Arrived")
+        self.products.append(self.getInput(ind))
         return(self.products)
-            
+
+    def getInput(self,ip):
+        inp = dict()
+        inner = dict()
+        print(ip)
+        for common in self.crendials['commons']:
+          inp[common] = input("Enter the {0}".format(common)) 
+        
+        for cat in self.crendials[ip]:
+          inner[cat] = input("Enter {0}".format(cat))
+        
+        inp.update({"product_details":inner})
+        
+        return(inp)
+
     def delete(self,id):
         for index,product in enumerate(self.products):
           if(product['id']==id):
             self.products.pop(index)
         self.display()
     
-# obj = Products([{
-#   "id": "1",
-#   "name": "Iphone 11",
-#   "product_details": {
-#     "ram": "2 GB",
-#     "processor": "snapdragon",
-#     "screen_size": "4 inch",
-#   },
-#   "cost": "50000",
-#   "currency": "INR",
-#   "category": "mobile",
-#   "colour": "black"
-# },
-# {
-#   "id": "2",
-#   "name": "Samsung Galaxy",
-#   "product_details": {
-#     "ram": "2 GB",
-#     "processor": "snapdragon",
-#     "screen_size": "4 inch",
-#   },
-#   "cost": "45000",
-#   "currency": "INR",
-#   "category": "mobile",
-#   "colour": "grey"
-# },
-# {
-#   "id": "3",
-#   "name": "Washing Machine",
-#   "product_details": {
-#     "machine_capacity": "6kg",
-#     "machine_rpm": "50",
-#     "type": "top_load"
-#   },
-#   "cost": "25000",
-#   "currency": "INR",
-#   "category": "washing-machine",
-#   "colour": "blue"
-# },
-# {
-#   "id": "4",
-#   "name": "Samsung TV",
-#   "product_details": {
-#     "isSmart": "false",
-#     "resolution": "UHD",
-#     "screen_size": "55 inch",
-#   },
-#   "cost": "40000",
-#   "currency": "INR",
-#   "category": "tv",
-#   "colour": "black"
-# }])
-
-# obj.list()
-# print(obj.add(1))
-# print(obj.delete(0))
 
