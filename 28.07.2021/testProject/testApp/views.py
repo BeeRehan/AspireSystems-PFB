865144fa16f746abb16b3304c168deb182e2a72b
unknown <beeyasin2014@gmail.com>
2021-08-14 10:46:44 +0530

    testApp

--- 28.07.2021/testProject/testApp/views.py
+++ 28.07.2021/testProject/testApp/views.py
@@ -4,20 +4,39 @@ from testApp.form import FormProductForm
 from .models import Products
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ProductsSerializer
 
 @login_required(login_url='/accounts/login')
 def index(request):
-    product = FormProductForm(request.POST or None)
-    if product.is_valid():
-        product.save()
-        return redirect('/show')  
+    product = FormProductForm()
+    # if product.is_valid():
+    #     product.save()
+    #     return redirect('/show')
+    # else:
+    #     print("Not added!!") 
     return render(request,"index.html",{'form':product})
+
+def add_product(request):
+    product = FormProductForm(request.POST or None)
+    if request.method == 'POST':
+        if product.is_valid():
+            product.save()
+            return redirect('/show')
+        else:
+            print("Not added!!")  
 
 @login_required(login_url='/accounts/login')
 def show(request):  
     products = Products.objects.all()  
     return render(request,"show.htm",{"things":products}) 
 
-def logout(request):
-    auth.logout(request)
-    return redirect('login')
+class ProductList(APIView):
+    def get(self,request):
+        products = Products.objects.all()
+        serializer = ProductsSerializer(products,many=True)
+        return Response(serializer.data)
+
+    
--- 28.07.2021/testProject/testProject/urls.py
+++ 28.07.2021/testProject/testProject/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from testApp import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('testApp/', include('testApp.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('index/', views.index),  
     path('show/',views.show),
+    path('ProductList/',views.ProductList.as_view()),
 ]
