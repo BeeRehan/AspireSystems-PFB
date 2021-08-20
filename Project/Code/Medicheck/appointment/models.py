0551eeafb6e56b574eef311167cc52d764f79c2f
Mohamed Mohamed <mohamed.mohamed@aspiresys.com>
2021-08-20 12:12:49 +0530

    Getting started with views

--- Project/Code/Medicheck/appointment/models.py
+++ Project/Code/Medicheck/appointment/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import SET_DEFAULT
 # Create your models here.
+class AppoinmentDetails(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    doctor = models.CharField(max_length=20)
+    status = models.CharField(max_length=20)
--- Project/Code/Medicheck/checkup/models.py
+++ Project/Code/Medicheck/checkup/models.py
@@ -1,5 +1,14 @@
 from django.db import models
-
+from appointment.models import AppoinmentDetails
 # Create your models here.
 class CheckupDetails(models.Model):
+    appointment = models.ForeignKey(AppoinmentDetails,on_delete=models.CASCADE)
+    temprature = models.CharField(max_length=10)
+    sugar_level  = models.CharField(max_length=10)
+    bp_level = models.CharField(max_length=10)
+    Advice = models.CharField(max_length=100)
+    prescription = models.CharField(max_length=100)
+    confirmed_diseases = models.CharField(max_length=100)
+
+    
     
