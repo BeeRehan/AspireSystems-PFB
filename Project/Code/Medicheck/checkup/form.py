8b8a2ceb01f768b95d5f2670f95930b18b5951a8
Mohamed Mohamed <mohamed.mohamed@aspiresys.com>
2021-08-23 18:23:06 +0530

    Completed!!!

--- Project/Code/Medicheck/checkup/form.py
+++ Project/Code/Medicheck/checkup/form.py
@@ -1,12 +1,13 @@
 from abc import ABCMeta
 from django import forms
 from django.forms.fields import CharField
+from django.forms.widgets import Textarea
 
 class checklistForm(forms.Form):
     #id = forms.IntegerField(label="Appoinment id")
     temprature = forms.CharField(label="Temprature",max_length=10)
     sugar_level = forms.CharField(label="Sugar Level",max_length=10)
     bp_level = forms.CharField(label="BP level",max_length=10)
-    advice = forms.CharField(label="Advice",max_length=120)
-    prescription = forms.CharField(label="Prescription",max_length=100)
+    advice = forms.CharField(label="Advice",widget=Textarea,max_length=120)
+    prescription = forms.CharField(label="Prescription",widget=Textarea,max_length=100)
     con_deseas = forms.CharField(label="Diseases",max_length=100)
