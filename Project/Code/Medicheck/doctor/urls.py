cd7fee98abc865330b377ec8304e0ad26f22c64d
BeeRehan <beeyasin2014@gmail.com>
2021-08-26 10:43:17 +0530

    Need to restructure it

--- Project/Code/Medicheck/doctor/urls.py
+++ Project/Code/Medicheck/doctor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index',views.index, name='doctor.index'),
+    path('',views.index, name='doctor.index'),
     path('checklist',views.checklist, name='doctor.checklist'),
     path('approval/<int:pk>',views.approval, name='doctor.approval')
 ]
